beeb8a478597f286ba9528a4f3684fc9f1995c35
Chunleshy <61976038+Chunleshy@users.noreply.github.com>
2023-12-09 15:55:01 -0600

    added devdiaries.onrender.com to allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = [https://devdiaries.onrender.com]
 # Setting for domain access on render.com
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost' 'devdiaries.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'devdiaries.onrender.com']
 
 
 # Application definition
